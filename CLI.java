5fe96c83f05f798f185ebb67c8ef7d3d1b6367e7
Rawan-Alnaggary <148374317+Rawan-Alnaggary@users.noreply.github.com>
2024-11-01 01:12:57 +0200

    Update CLI.java

--- CLI.java
+++ CLI.java
@@ -15,43 +15,28 @@ public class CLI
             System.out.print(System.getProperty("user.dir"));
             System.out.print("> ");
             commandLine = sc.nextLine();
-            String[] command = commandLine.split(" ");
-            switch (command[0])
+            if (commandLine.contains("|"))
             {
-                case "pwd":
-                    Commands.pwd();
-                    break;
+                String[] command = commandLine.split("\\|");
+                Commands.pipe(command);
+            }
+            else
+            {
+                String[] command = commandLine.split(" ");
+                switch (command[0])
+                {
+                    case "pwd":
+                        Commands.pwd();
+                        break;
 
-                case "cd":
-                    Commands.cd(command[1]);
-                    break;
+                    case "cd":
+                        Commands.cd(command[1]);
+                        break;
 
-                case "ls":
-                    if (command.length == 1)
-                    {
-                        String[] files = Commands.ls();
-                        for (String file : files)
+                    case "ls":
+                        if (command.length == 1)
                         {
-                            System.out.print(file);
-                            System.out.print("  ");
-                        }
-                        System.out.print('\n');
-                    }
-                    else
-                    {
-                        if (command[1].equals("-a"))
-                        {
-                            String[] files = Commands.lsA();
-                            for (String file : files)
-                            {
-                                System.out.print(file);
-                                System.out.print("  ");
-                            }
-                            System.out.print('\n');
-                        }
-                        else if (command[1].equals("-r"))
-                        {
-                            String[] files = Commands.lsR();
+                            String[] files = Commands.ls();
                             for (String file : files)
                             {
                                 System.out.print(file);
@@ -60,60 +45,81 @@ public class CLI
                             System.out.print('\n');
                         }
                         else
-                            System.out.println("Unknown command");
-                    }
-                    break;
+                        {
+                            if (command[1].equals("-a"))
+                            {
+                                String[] files = Commands.lsA();
+                                for (String file : files)
+                                {
+                                    System.out.print(file);
+                                    System.out.print("  ");
+                                }
+                                System.out.print('\n');
+                            }
+                            else if (command[1].equals("-r"))
+                            {
+                                String[] files = Commands.lsR();
+                                for (String file : files)
+                                {
+                                    System.out.print(file);
+                                    System.out.print("  ");
+                                }
+                                System.out.print('\n');
+                            }
+                            else
+                                System.out.println("Unknown command");
+                        }
+                        break;
 
-                case "mkdir":
-                    Commands.mkdir(command);
-                    break;
+                    case "mkdir":
+                        Commands.mkdir(command);
+                        break;
 
-                case "rmdir":
-                    Commands.rmdir(command);
-                    break;
+                    case "rmdir":
+                        Commands.rmdir(command);
+                        break;
 
-                case "touch":
-                    Commands.touch(command);
-                    break;
+                    case "touch":
+                        Commands.touch(command);
+                        break;
 
-                case "mv":
-                    if (command.length > 2)
-                        Commands.mv(command);
-                    else
-                        System.out.println("No destination file specified");
-                    break;
+                    case "mv":
+                        if (command.length > 2)
+                            Commands.mv(command);
+                        else
+                            System.out.println("No destination file specified");
+                        break;
 
-                case "rm":
-                    Commands.rm(command);
-                    break;
+                    case "rm":
+                        Commands.rm(command);
+                        break;
 
-                case "cat":
-                    if (command[2].equals(">"))
-                        Commands.catWrite(command[1]);
-                    else if (command[2].equals(">>"))
-                        Commands.catAppend(command[1]);
-                    else
-                        Commands.cat(command);
-                    break;
+                    case "cat":
+                        if (command[2].equals(">"))
+                            Commands.catWrite(command[1]);
+                        else if (command[2].equals(">>"))
+                            Commands.catAppend(command[1]);
+                        else
+                            Commands.cat(command);
+                        break;
 
-                case "sort":
-                    Commands.sort(command[1]);
-                    break;
+                    case "sort":
+                        String[] lines = Commands.sort(command[1]);
+                        for (String line : lines)
+                            System.out.println(line);
+                        break;
 
-                case "uniq":
-                    Commands.uniq(command[1]);
-                    break;
+                    case "help":
+                        Commands.help();
+                        break;
 
-                case "help":
-                    Commands.help();
-                    break;
+                    case "exit":
+                        quit = true;
+                        break;
 
-                case "exit":
-                    quit = true;
-                    break;
-
-                default:
-                    System.out.println("Unknown command");
+                    default:
+                        System.out.println("Unknown command");
+                }
             }
         }
     }
